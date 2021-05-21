3aa85b7417ff6dc86177de2a87d0b52a13c58022
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-05-21 14:34:01 +0300

    added products section

--- app/js/main.js
+++ app/js/main.js
@@ -2,7 +2,11 @@ $(function(){
  $('.top-slider__inner').slick({
    arrows: false,
    dots: true,
-   fade: true
-
+   fade: true,
+   autoplay: true,
+   autoplaySpeed: 2000
  })
+
+ var mixer = mixitup('.products__items');
 });
+

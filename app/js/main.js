38d4ca5beb2b93a34717f2aff4c6a762f98710b6
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-05-23 19:26:38 +0300

    fixed details

--- app/js/main.js
+++ app/js/main.js
@@ -9,7 +9,7 @@ $(function(){
 
 
 var containerEl1 = document.querySelector('[data-ref="container-1"]');
-  var containerEl2 = document.querySelector('[data-ref="container-2"]');
+var containerEl2 = document.querySelector('[data-ref="container-2"]');
  
   var config = {
     controls: {

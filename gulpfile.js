70c343936dbcd29f0a5f4b104b0d06a500298aff
nikolaos-franklin <katya.nikolaos17@gmail.com>
2021-06-01 16:02:16 +0300

    added product list page

--- gulpfile.js
+++ gulpfile.js
@@ -32,12 +32,14 @@ function styles() {
   .pipe(browserSync.stream())
 }
 
-function scripts(){
+function scripts(){ 
   return src([
     'node_modules/jquery/dist/jquery.js',
     'node_modules/slick-carousel/slick/slick.js',
     'node_modules/mixitup/dist/mixitup.js',
     'node_modules/@fancyapps/fancybox/dist/jquery.fancybox.js',
+    'node_modules/ion-rangeslider/js/ion.rangeSlider.js',
+    'node_modules/rateyo/src/jquery.rateyo.js',
     'app/js/main.js'
   ])
   .pipe(concat('main.min.js'))
